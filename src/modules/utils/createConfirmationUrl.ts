57c80b4223611ba5abf365661f9d5191f60f4d90
eddie naff <eddienaff@eddienaff.com>
2019-04-03 12:04:22 -0700

    Remove uuid from confirmation email link, replace with user ID for login process

--- src/modules/utils/createConfirmationUrl.ts
+++ src/modules/utils/createConfirmationUrl.ts
@@ -1,10 +1,10 @@
-import { v4 } from "uuid";
+// import { v4 } from "uuid";
 import { redis } from "../../redis";
 import { confirmUserPrefix } from "../constants/redisPrefixes";
 
 export const createConfirmationUrl = async (userId: string) => {
-  const token = v4();
+  // const token = v4();
   await redis.set(confirmUserPrefix + userId, userId, "ex", 60 * 60 * 24); // 1 day expiration
 
-  return `http://localhost:3000/user/confirm/${token}`;
+  return `http://localhost:3000/user/confirm/${userId}`;
 };
