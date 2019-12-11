a0ef89ee160fa6aaf7fcf349a43dffa860dc5c5a
eddie naff <eddienaff@eddienaff.com>
2019-12-10 21:46:30 -0800

    Add pretty-print to logger

--- src/modules/middleware/logger.ts
+++ src/modules/middleware/logger.ts
@@ -1,9 +1,11 @@
 import { MiddlewareFn } from "type-graphql";
 import { MyContext } from "../../types/MyContext";
 
-import logger from "pino";
+import pino from "pino";
 
-// const { log } = console;
+const logger = pino({
+  prettyPrint: { colorize: true }
+});
 
 export const loggerMiddleware: MiddlewareFn<MyContext> = async (
   { info },
@@ -13,19 +15,9 @@ export const loggerMiddleware: MiddlewareFn<MyContext> = async (
   await next();
   const resolveTime = Date.now() - start;
 
-  // console.log(`${info.parentType.name}.${info.fieldName} [${resolveTime} ms]`);
-
-  // logger().info({
-  //   type: "operation",
-  //   name: args,
-  //   ms: elapsedTimeInMs
-  // });
-
-  logger().info({
+  logger.info({
     type: "timing",
     name: `${info.parentType.name}.${info.fieldName}`,
     ms: `${resolveTime} ms`
   });
-
-  // return next();
 };
