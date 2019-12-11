eb9f34029b939400a4a297bd9c60453be07c073b
eddie naff <eddienaff@eddienaff.com>
2019-12-10 21:46:45 -0800

    Improve types

--- src/types/MyContext.ts
+++ src/types/MyContext.ts
@@ -1,6 +1,14 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import { GraphQLResolveInfo, GraphQLArgs } from "graphql";
+
+interface GraphQlInputs {
+  args: GraphQLArgs;
+  info: GraphQLResolveInfo;
+}
 
 export interface MyContext {
+  gqlOpts: GraphQlInputs;
   req: Request;
   res: Response;
+  next: NextFunction;
 }
